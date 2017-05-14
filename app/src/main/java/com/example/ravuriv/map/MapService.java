12cd4f16a6f100a8b152bf38cb7cd25dcaf7ca75
Kailash Ravuri <Kailash.Ravuri@Parexel.com>
2017-05-14 17:14:31 +0100

    Initial commit of the app

---
+++ app/src/main/java/com/example/ravuriv/map/MapService.java
@@ -0,0 +1,158 @@
+package com.example.ravuriv.map;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import android.location.LocationListener;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import static com.example.ravuriv.map.R.id.map;
+
+public class MapService extends Service implements LocationListener {
+    private static MapService mMapService;
+    private Context mContext;
+    LocationManager mLocationManager;
+    // Flag for GPS status
+    boolean isGPSEnabled = false;
+    boolean isTrackingOn = true;
+    // Flag for network status
+    boolean isNetworkEnabled = false;
+    // Flag for GPS status
+    boolean canGetLocation = false;
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    double latitude, prevLatitude;
+    double longitude, prevLongitude;
+    Location mLocation;
+    MapsActivity mapsActivity;
+
+    public MapService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mMapService = this;
+        mContext = this;
+        getLocation();
+        mapsActivity = new MapsActivity();
+    }
+    public Location getLocation() {
+        try {
+            mLocationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // Getting GPS status
+            isGPSEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // Getting network status
+            isNetworkEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // No network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return null;
+                    }
+                    mLocationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (mLocationManager != null) {
+                        mLocation = mLocationManager
+                                .getLastKnownLocation(mLocationManager.NETWORK_PROVIDER);
+                        if (mLocation != null) {
+                            latitude = mLocation.getLatitude();
+                            longitude = mLocation.getLongitude();
+                        }
+                    }
+                }
+                // If GPS enabled, get latitude/longitude using GPS Services
+                if (isGPSEnabled) {
+                    if (mLocation == null) {
+                        mLocationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (mLocationManager != null) {
+                            mLocation = mLocationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (mLocation != null) {
+                                latitude = mLocation.getLatitude();
+                                longitude = mLocation.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mLocation;
+    }
+    public static MapService getMapService(){
+        return mMapService;
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(!mapsActivity.isActivityPaused){
+            prevLatitude = latitude;
+            prevLongitude = longitude;
+        }
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        mapsActivity.moveCamera(latitude,longitude);
+        if(isTrackingOn && !mapsActivity.isActivityPaused){
+            mapsActivity.drawLine(latitude,longitude);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/ravuriv/map/MapsActivity.java
@@ -0,0 +1,146 @@
+package com.example.ravuriv.map;
+
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+
+import static com.example.ravuriv.map.R.id.map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    MapService mapService;
+    private Polyline line;
+    private ArrayList<LatLng> routePoints;
+    boolean isActivityPaused = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        askForPermission();
+        isActivityPaused = false;
+        if(mapService.isTrackingOn){
+            mMap.addPolyline(new PolylineOptions()
+                    .add(new LatLng(mapService.prevLatitude,mapService.prevLongitude),new LatLng(mapService.latitude,mapService.longitude))
+                    .width(5)
+                    .color(Color.BLUE));
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mapService.latitude,mapService.longitude),17.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mapService.latitude,mapService.longitude),17.0f));
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isActivityPaused = true;
+    }
+
+    /**
+ * Manipulates the map once available.
+ * This callback is triggered when the map is ready to be used.
+ * This is where we can add markers or lines, add listeners or move the camera. In this case,
+ * we just add a marker near Sydney, Australia.
+ * If Google Play services is not installed on the device, the user will be prompted to install
+ * it inside the SupportMapFragment. This method will only be triggered once the user has
+ * installed Google Play services and returned to the app.
+ */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if(MapService.getMapService()==null){
+            Intent intent = new Intent(this,MapService.class);
+            startService(intent);
+            //bindService(intent,mConnection, Context.BIND_AUTO_CREATE);
+        }
+        mapService = MapService.getMapService();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            askForPermission();
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+        Location location = null;
+        if(mapService.canGetLocation) {
+            location = mapService.getLocation();
+            Log.i("Check Log","can get location");
+
+        }
+        if(location !=null) {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,17.0f));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17.0f));
+            Log.i("Check Log","mark");
+        }
+    }
+    public void moveCamera(double lat, double lon){
+        LatLng latLng = new LatLng(lat,lon);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17.0f));
+    }
+    public void drawLine(double lat, double lon){
+        LatLng latLng = new LatLng(lat,lon);
+        PolylineOptions pOptions = new PolylineOptions()
+                .width(5)
+                .color(Color.BLUE)
+                .geodesic(true);
+        for (int z = 0; z < routePoints.size(); z++) {
+            LatLng point = routePoints.get(z);
+            pOptions.add(point);
+        }
+        line = mMap.addPolyline(pOptions);
+        routePoints.add(latLng);
+
+    }
+    private void askForPermission() {
+        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        ActivityCompat.requestPermissions(this, permissions,1);
+    }
+
+
+    protected ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+}
