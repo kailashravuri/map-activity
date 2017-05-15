f77d7b306d5d6e0ccc3571514eae173c24142cc5
Kailash Ravuri <Kailash.Ravuri@Parexel.com>
2017-05-15 22:08:29 +0100

    trying to refactor the code

--- app/src/main/java/common/map/utils/LocationUtils.java
+++ app/src/main/java/common/map/utils/LocationUtils.java
@@ -1,8 +1,100 @@
 package common.map.utils;
 
-/**
- * Created by RavuriV on 15/05/2017.
- */
+
+import android.Manifest;
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import static android.content.Context.LOCATION_SERVICE;
+import static android.location.LocationManager.NETWORK_PROVIDER;
 
 public class LocationUtils {
+
+    private LocationManager mLocationManager;
+    private AccessibilityService mContext;
+    public boolean iIsGPSEnabled;
+    public boolean isNetworkEnabled;
+    public boolean canGetLocation;
+    public double latitude;
+    public double longitude;
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 10; // 1 minute
+
+    public Location getLocation(Location mLocation , Context locationListner) {
+        try {
+            mLocationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // Getting GPS status
+            iIsGPSEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // Getting network status
+            isNetworkEnabled = mLocationManager
+                    .isProviderEnabled(NETWORK_PROVIDER);
+
+            if (!iIsGPSEnabled && !isNetworkEnabled) {
+                // TODO: No network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(locationListner, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return null;
+                    }
+                    mLocationManager.requestLocationUpdates(
+                            NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) locationListner);
+                    Log.d("Network", "Network");
+                    if (mLocationManager != null) {
+                        mLocation = mLocationManager
+                                .getLastKnownLocation(NETWORK_PROVIDER);
+                        if (mLocation != null) {
+                            latitude = mLocation.getlatitude();
+                            longitude = mLocation.getlongitude();
+                        }
+                    }
+                }
+                // If GPS enabled, get latitude/longitude using GPS Services
+                if (iIsGPSEnabled) {
+                    if (mLocation == null) {
+                        mLocationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) locationListner);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (mLocationManager != null) {
+                            mLocation = mLocationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (mLocation != null) {
+                                latitude = mLocation.getlatitude();
+                                longitude = mLocation.getlongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mLocation;
+    }
 }
